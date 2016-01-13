d9b99520528f28877561f21fe5952509f7f7e578
xmy0010 <www.18144200589@163.com>
2016-01-13 09:51:51 +0800

    添加工程所需素材

---
+++ AudioResources/UrlSourceApi.h
@@ -0,0 +1,30 @@
+//
+//  UrlSourceApi.h
+//  1505AudioDemo
+//
+//  Created by HeHui on 16/1/12.
+//  Copyright © 2016年 Hawie. All rights reserved.
+//
+
+#ifndef UrlSourceApi_h
+#define UrlSourceApi_h
+
+#define url0 @"http://7xq0yl.media1.z0.glb.clouddn.com/Fools%20Garden%20-%20Lemon%20Tree.mp3" // LemonTree
+
+#define url1 @"http://7xq0yl.media1.z0.glb.clouddn.com/John%20Denver%20-%20Take%20Me%20Home%20Country%20Roads.mp3" // Take me home country roads
+
+#define url2 @"http://7xq0yl.media1.z0.glb.clouddn.com/Maroon%205%20-%20Sugar.mp3"  // Sugar
+
+#define url3 @"http://7xq0yl.media1.z0.glb.clouddn.com/BAAD%20-%20%E5%90%9B%E3%81%8C%E5%A5%BD%E3%81%8D%E3%81%A0%E3%81%A8%E5%8F%AB%E3%81%B3%E3%81%9F%E3%81%84.mp3" // 好像大声叫喜欢你
+
+#define url4 @"http://7xq0yl.media1.z0.glb.clouddn.com/WANDS%20-%20%E4%B8%96%E7%95%8C%E3%81%8C%E7%BB%88%E3%82%8F%E3%82%8B%E3%81%BE%E3%81%A7%E3%81%AF%E2%80%A6.mp3" // 直到世界尽头
+
+
+
+#endif /* UrlSourceApi_h */
+
+/*
+ *NSArray *nameArr = @[@"Fools Garden - Lemon Tree",@"John Denver - Take Me Home Country Roads",@"Maroon 5 - Sugar",@"WANDS - 世界が终わるまでは…",@"BAAD - 君が好きだと叫びたい"];
+ */
+
+
